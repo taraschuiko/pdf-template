ca5f316ec4d86d57bf3b1571d1d6c31e348db1a5
Taras Chuiko <taraschuiko@gmail.com>
2020-08-20 11:50:37 +0300

    Made loading and downloading file

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,30 @@
         <p>Company logo</p>
         <input type="file" name="logo">
       </label>
-      <button>Download</button>
+      <button @click="download">Download</button>
     </form>
   </div>
 </template>
 
 <script>
+import { PDFDocument } from 'pdf-lib';
+import inBrowserDownload from 'in-browser-download';
+
 export default {
   name: 'App',
   components: {
+  },
+  methods: {
+    async download(e) {
+      e.preventDefault();
+
+      const templateBytesLight = new Uint8Array(
+        await fetch('/templates/master_light.pdf').then((r) => r.arrayBuffer()),
+      );
+      const pdfLight = await PDFDocument.load(templateBytesLight);
+
+      inBrowserDownload((await pdfLight.save()).buffer, 'light.pdf');
+    },
   },
 };
 </script>
